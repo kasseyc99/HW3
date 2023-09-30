93514e71736592f3865507df3b7177f44894dca9
kasseyc99 <51234776+kasseyc99@users.noreply.github.com>
2023-09-30 18:50:45 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@hw-31/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+
+int a;
+
+  cout << "Type one integer here." << endl;
+
+  cin >> a;
+
+  for (int i = 0; i<= a; i++)
+
+  {
+
+    cout << i;
+  }
+  cout << endl;
+  return 0;
+}
+  
---
+++ exercise1.cpp
@@ -0,0 +1,29 @@
+//***********************************************************
+// Name: <Your name here>
+// Title: <File Name>.cpp
+// Description: Short description should be typed here.
+// Date: 9/29/2023
+//***********************************************************
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+
+int a;
+
+  cout << "Type one integer here." << endl;
+
+  cin >> a;
+
+  for (int i = 0; i<= a; i++)
+
+  {
+
+    cout << i;
+  }
+  cout << endl;
+  return 0;
+}
+  
