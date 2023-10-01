557a6124fdcef42f1e0bb13639b2966caa572e02
kasseyc99 <51234776+kasseyc99@users.noreply.github.com>
2023-10-01 00:04:03 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@exercise/exercise4.cpp
@@ -0,0 +1,39 @@
+//***********************************************************
+// Name: <kassem chahrour>
+// Title: <exercise4>.cpp
+// Description: integer calculator 
+// Date: 9/29/2023
+//***********************************************************
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int userinput;
+  
+  int sum =0;
+  
+bool a = true;
+  while ( a) 
+    {
+      cout << " type one postive integer or -1 (quit) " ;
+      cin>> userinput;
+      
+      if (userinput == -1) {
+        
+          break;
+      
+      }
+     else {
+       sum = sum + userinput; 
+     
+     }
+      
+         
+     }
+     
+  cout<< " Total:" << sum << endl;
+ return 0; 
+  
+}
